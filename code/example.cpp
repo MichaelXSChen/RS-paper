5f51366fa55080c224ed36be088619623731de06
heming <heming@cs.columbia.edu>
2016-04-23 01:47:32 +0800

    init

---
+++ code/example.cpp
@@ -0,0 +1,25 @@
+int main(int argc, char **argv) {
+ FILE *input_desc;
+ int argind = 1;
+ const char *infile = "-";
+ do { // iterate over input files and print one by one
+   if(argind < argc)
+     infile = argv[argind];
+   if(strcmp(infile, "-")) // input is a file
+     input_desc = fopen(infile, "r");
+   else // input is @stdin@
+     input_desc = stdin;
+   if(!input_desc) continue;
+   int c;
+   while((c = fgetc(input_desc)) != EOF) {
+     if(c < 32 && c != '\n') { // non-printable char
+       putchar('^');
+       putchar(c + 64);
+     } else // printable char
+       putchar(c);
+   }
+   if(infile[0] != '-' || infile[1] != 0)
+     fclose(input_desc); // input is a file
+ } while (++argind < argc);
+ return 0;
+}
