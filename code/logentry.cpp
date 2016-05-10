94c4ab55e46b392aafb75c34142576343c860fcd
heming <heming@cs.columbia.edu>
2016-05-10 22:22:47 +0800

    typo

--- code/logentry.cpp
+++ code/logentry.cpp
@@ -1,9 +1,9 @@
 struct log_entry_t {
-  consensus_ack ack[MAX]; // Output hash
+  consensus_ack ack[MAX]; // Per replica output hash.
   viewstamp_t vs;
   viewstamp_t last_committed;
   int node_id;
-  viewstamp_t conn_vs; // viewstamp when connection was accepted.
+  viewstamp_t conn_vs; // client connection ID.
   int call_type; // socket call type.
   size_t data_sz; // data size in the call.
   char data[0]; // data, with canary value in last byte.
